2ccc6b16fe3bdab5f768d8a6b4ce7803107f8a05
mbroderson <m.brodecki@binarapps.com>
2015-09-22 15:03:24 +0200

    Env changed

--- configurations/reservation_slack_config.rb
+++ configurations/reservation_slack_config.rb
@@ -1,6 +1,6 @@
 class ReservationSlackConfig
     CONFIG = {
-      'channel'          =>  ENV.fetch('CHANNEL'),
+      'channel'          =>  ENV.fetch('RESERVATION_CHANNEL'),
       'name'             => 'botsy',
       'incoming_webhook' =>  ENV.fetch('INCOMING_WEBHOOK'),
       'outgoing_token'   =>  ENV.fetch('OUTGOING_TOKEN')
