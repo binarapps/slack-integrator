e952da71e3fac01e67a9ae31bf36576f71afb8e7
mbroderson <m.brodecki@binarapps.com>
2015-09-22 13:45:44 +0200

    Outgoing token added

--- configurations/reservation_slack_config.rb
+++ configurations/reservation_slack_config.rb
@@ -3,6 +3,6 @@ class ReservationSlackConfig
       'channel'          =>  ENV.fetch('CHANNEL'),
       'name'             => 'botsy',
       'incoming_webhook' =>  ENV.fetch('INCOMING_WEBHOOK'),
-      'outgoing_token'   => 'secret'
+      'outgoing_token'   =>  ENV.fetch('OUTGOING_TOKEN')
     }
 end
