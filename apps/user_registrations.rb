8e6cee2ef3e2528ea049abcad9726243271589b0
mbroderson <m.brodecki@binarapps.com>
2015-09-30 15:33:06 +0200

    Filtering users by yml file

--- apps/user_registrations.rb
+++ apps/user_registrations.rb
@@ -1,8 +1,8 @@
 RodaApp.route('user_registrations') do |r|
   r.post 'create' do
     begin
-      RegisterUser.new.(r.params)
-    rescue Sequel::ValidationFailed => e
+      RegisterUser.new(r.params).call
+    rescue Sequel::ValidationFailed, UnauthorizedUser => e
       flash[:error] = "Errors occured"
       r.redirect '/user_registrations/new'
     end
--- services/register_user.rb
+++ services/register_user.rb
@@ -1,8 +1,25 @@
+require 'yaml'
+
+class UnauthorizedUser < Exception; end
+
 class RegisterUser
 
-  def call(params)
-    User.create(email: params['email'],
-                password: params['password'],
-                password_confirmation: params['password_confirmation'])
+  def initialize(params)
+    @email = params['email']
+    @password = params['password']
+    @password_confirmation = params['password_confirmation']
+  end
+
+  def call
+    authorize_specific_users
+    User.create(email: @email,
+                password: @password,
+                password_confirmation: @password_confirmation)
+  end
+
+  private
+  def authorize_specific_users
+    yml = YAML.load_file('configurations/authorized_users.yml')
+    raise UnauthorizedUser if yml[@email].nil?
   end
 end
