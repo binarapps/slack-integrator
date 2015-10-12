d88e1cc6446cc03cdb7f23735ee718ba325eaf80
Maciej Brodecki <ma.brodecki@binarapps.com>
2015-10-12 13:40:02 +0200

    spec fix

--- spec/unit/reservation_list_spec.rb
+++ spec/unit/reservation_list_spec.rb
@@ -8,7 +8,7 @@ describe "Paginated list" do
     FactoryGirl.create(:reservation, from: '10', to: '11')
     FactoryGirl.create(:reservation, from: '12', to: '13')
     FactoryGirl.create(:reservation, from: '14', to: '15')
-    Reservation.create(reserved_at: (Time.now + 86400), from: '10', to: '11')
+    FactoryGirl.create(:reservation, reserved_at: (Time.now + 86400), from: '10', to: '11')
 
     expect(Reservation.by_day(Time.now).count).to match(3)
     expect(Reservation.by_day(Time.now + 86400).count).to match(1)
