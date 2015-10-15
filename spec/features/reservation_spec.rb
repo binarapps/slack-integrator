4900587fcd3d5d60cc535c278d621b24472f72b3
Maciej Brodecki <ma.brodecki@binarapps.com>
2015-10-15 14:27:22 +0200

    hound fixes

--- spec/features/reservation_spec.rb
+++ spec/features/reservation_spec.rb
@@ -2,38 +2,38 @@ require 'spec_helper'
 
 describe "reservation actions", :type => :feature, :js => true do
 
-  let(:user){ FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
 
-  it "creates new reservation" do
+  it 'creates new reservation' do
     login_user(user)
     visit '/'
     click_button 'Create new reservation'
-    fill_in 'from', :with => '11'
-    fill_in 'to', :with => '12'
+    fill_in 'from', with: '11'
+    fill_in 'to', with: '12'
 
     click_button 'Save'
     expect(Reservation.count).to match(1)
   end
 
-  it "returns reservation alreay exists error" do
+  it 'returns reservation alreay exists error' do
     login_user(user)
     FactoryGirl.create(:reservation, from: '11', to: '12')
     visit '/'
     click_button 'Create new reservation'
-    fill_in 'from', :with => '11'
-    fill_in 'to', :with => '12'
+    fill_in 'from', with: '11'
+    fill_in 'to', with: '12'
 
     click_button 'Save'
     expect(Reservation.count).to match(1)
   end
 
-  it "returns empty params error" do
+  it 'returns empty params error' do
     login_user(user)
     FactoryGirl.create(:reservation, from: '11', to: '12')
     visit '/'
     click_button 'Create new reservation'
-    fill_in 'from', :with => ''
-    fill_in 'to', :with => ''
+    fill_in 'from', with: ''
+    fill_in 'to', with: ''
 
     click_button 'Save'
     expect(Reservation.count).to match(1)
--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,7 +1,7 @@
 def login_user(user)
   visit '/user_sessions/new'
-  fill_in 'email', :with => user.email
-  fill_in 'password', :with => user.password
+  fill_in 'email', with: user.email
+  fill_in 'password', with: user.password
 
   click_button 'Log in'
   expect(page).to have_content 'Logout'
