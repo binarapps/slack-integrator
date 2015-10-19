7d27e570661e92137a6a4e5cdf25886c63c7598d
Maciej Brodecki <ma.brodecki@binarapps.com>
2015-10-19 12:47:41 +0200

    tests improvement

--- spec/features/reservation_spec.rb
+++ spec/features/reservation_spec.rb
@@ -4,9 +4,9 @@ describe "reservation actions", :type => :feature, :js => true do
 
   let(:user) { FactoryGirl.create(:user) }
 
-  before(:each) {
+  before(:each) do
     login_user(user)
-  }
+  end
 
   it 'creates new reservation' do
     visit '/'
@@ -14,6 +14,7 @@ describe "reservation actions", :type => :feature, :js => true do
     fill_in 'from', with: '11'
     fill_in 'to', with: '12'
 
+    expect(Reservation.count).to match(0)
     click_button 'Save'
     expect(Reservation.count).to match(1)
   end
@@ -25,18 +26,21 @@ describe "reservation actions", :type => :feature, :js => true do
     fill_in 'from', with: '11'
     fill_in 'to', with: '12'
 
+    expect(Reservation.count).to match(1)
     click_button 'Save'
+    expect(page).to have_content 'Sorry, errors occured: DateAlreadyReserved'
     expect(Reservation.count).to match(1)
   end
 
   it 'returns empty params error' do
-    FactoryGirl.create(:reservation, from: '11', to: '12')
     visit '/'
     click_button 'Create new reservation'
     fill_in 'from', with: ''
     fill_in 'to', with: ''
 
+    expect(Reservation.count).to match(0)
     click_button 'Save'
-    expect(Reservation.count).to match(1)
+    expect(page).to have_content 'Sorry, errors occured: from is not present, to is not present'
+    expect(Reservation.count).to match(0)
   end
 end
