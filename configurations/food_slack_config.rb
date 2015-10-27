cc18d72cdef2fb52238f0e3f313979502c4cb8fa
Maciej Brodecki <ma.brodecki@binarapps.com>
2015-10-27 11:43:59 +0100

    test fix

--- configurations/food_slack_config.rb
+++ configurations/food_slack_config.rb
@@ -1,8 +1,10 @@
 class FoodSlackConfig
-  CONFIG = {
-    'channel'          =>  ENV.fetch('FOOD_ORDER_CHANNEL'),
-    'name'             => 'food',
-    'incoming_webhook' =>  ENV.fetch('FOOD_ORDER_INCOMING_WEBHOOK'),
-    'outgoing_token'   =>  ENV.fetch('FOOD_ORDER_OUTGOING_TOKEN')
-  }
+  unless test?
+    CONFIG = {
+      'channel'          =>  ENV.fetch('FOOD_ORDER_CHANNEL'),
+      'name'             => 'food',
+      'incoming_webhook' =>  ENV.fetch('FOOD_ORDER_INCOMING_WEBHOOK'),
+      'outgoing_token'   =>  ENV.fetch('FOOD_ORDER_OUTGOING_TOKEN')
+    }
+  end
 end
