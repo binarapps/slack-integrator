304595881b76056476417c475b6afdf8cc860aca
Michal Gutowski <m.gutowski@binarapps.com>
2016-03-17 17:02:30 +0100

    Migrations problem
    migration name replaces whitespaces with _
    and downcase the name

--- tasks/generate.rb
+++ tasks/generate.rb
@@ -7,9 +7,10 @@ namespace :generate do
       exit false
     end
 
+    migration_name = args[:name].downcase.split.join('_') #remove whitespace
     content = "Sequel.migration do\n  up do\n    \n  end\n\n  down do\n    \n  end\nend\n"
     timestamp = DateTime.now.strftime('%Y%m%d%H%M%S')
-    filename = File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'migrations', "#{timestamp}_#{args[:name]}.rb")
+    filename = File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'migrations', "#{timestamp}_#{migration_name}.rb")
 
     File.open(filename, 'w') do |f|
       f.puts content
